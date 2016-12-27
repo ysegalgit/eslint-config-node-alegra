27d30e6cae0510f0f399c1de31e3f8c2927136f2
YaronSegal <seg.yar@gmail.com>
2016-12-27 09:59:10 +0200

    updated rules list

--- index.js
+++ index.js
@@ -4,8 +4,49 @@ module.exports = {
         MyGlobal: true
     },
 
+    env: {
+         "node": true
+    },
+
     rules: {
-        semi: ["error", "always"]
+        //Possible errors
+        "no-unsafe-negation" : "error",
+        "no-extra-parens" : "error",
+        "no-console" : "off",
+        "no-constant-condition" : "error",
+        //TODO
+
+        //Best Practices
+        "complexity" : ["error", 3],
+        //TODO
+
+        //Strict Mode
+        "strict" : "error",
+
+        //Variables
+        "no-unused-vars": ["error", { "varsIgnorePattern": "cl" }],
+        //TODO
+
+        //Node.js and CommonJS
+        //TODO
+
+        //Stylistic Issues
+        "linebreak-style" : "off",
+        "semi": ["error", "always"],
+        "quotes": "off",
+        "max-len" : ["error" ,{ "ignoreComments": true ,"code" : 120}],
+        "comma-spacing" : "off",
+        "space-before-blocks" : ["error" , "never"],
+        "indent": ["error", "tab"]
+        //TODO
+
+        //ECMAScript 6
+        //TODO
+
+        //Deprecated
+        //TODO
+
+
     }
 
 };
